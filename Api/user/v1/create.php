cf1d498bad9a776e77bc74ebfaff7803b1432b07
Sagar Mishra <sagar.mishra2048@gmail.com>
2023-03-08 19:40:25 +1000

    Code optimize
    We are removing else condition because in this code we are redirecting with `header()` and `exit()` so  execution flow will never move to next line.  It is best practice to minimise `else()` block when ever we can

--- Api/user/v1/create.php
+++ Api/user/v1/create.php
@@ -16,31 +16,28 @@ $connection = $db->connect();
 $user = new  User($connection);
 
 
-
-if ($_SERVER['REQUEST_METHOD'] === "POST") {
-    //submit data
-
-    $user->name = $_POST['name'];
-    $user->email = $_POST['email'];
-    $user->password = $_POST['password'];
-    $user->role = $_POST['role'];
-    $user->description = $_POST['description'];
-
-
-
-
-    if ($user->create_data()) {
-        $message = "Successfuly has been created";
-        header("location: http://localhost:8100/Show/Api/Front/Dash.php?message=$message");
-        exit;
-    } else {
-        $message = "Failed to insert data";
-        header("location: http://localhost:8100/Show/Api/Front/Dash.php?message=$message");
-        exit;
-    }
-} else {
-
+if (!$_SERVER['REQUEST_METHOD'] === "POST") {
     $message = "Access denied";
     header("location: http://localhost:8100/Show/Api/Front/Dash.php?message=$message");
     exit;
 }
+    //submit data
+
+$user->name = $_POST['name'];
+$user->email = $_POST['email'];
+$user->password = $_POST['password'];
+$user->role = $_POST['role'];
+$user->description = $_POST['description'];
+
+
+if ($user->create_data()) {
+    $message = "Successfuly has been created";
+    header("location: http://localhost:8100/Show/Api/Front/Dash.php?message=$message");
+    exit;
+}
+
+$message = "Failed to insert data";
+header("location: http://localhost:8100/Show/Api/Front/Dash.php?message=$message");
+exit;
+   
+
